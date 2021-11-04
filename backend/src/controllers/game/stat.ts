edbb8c7d9df5a3213305a9f98c538cb64c5997ab
Witchayut Kornsuwannawit <boss.wt13@gmail.com>
2021-11-05 02:42:20 +0700

    🐛 matchStart undefined handle

--- backend/src/controllers/game/stat.ts
+++ backend/src/controllers/game/stat.ts
@@ -4,36 +4,45 @@ import { findOpponentSocketId } from '../../utils';
 
 export const stat = (socket: Socket, room: Room) => {
 	// Create a list and store open rooms with only Host Username and Room ID properties
-    const opponentSocketId = findOpponentSocketId(room, socket.id);
+	const opponentSocketId = findOpponentSocketId(room, socket.id);
 
 	socket.emit(
-        'statResponse',
-        'Completed',
-        room.hostShot.length,
-        room.hostHitCount,
-        room.hostShot.length - room.hostHitCount,
-        room.hostShot.length != 0 ? room.hostHitCount/room.hostShot.length * 100: room.hostHitCount * 100,
-        room.guestShot.length,
-        room.guestHitCount,
-        room.guestShot.length - room.guestHitCount,
-        room.guestShot.length != 0 ? room.guestHitCount/room.guestShot.length * 100: room.guestHitCount * 100,
-        Date.now() - room.matchStart,
-        room.turnCount
-    )
+		'statResponse',
+		'Completed',
+		room.hostShot.length,
+		room.hostHitCount,
+		room.hostShot.length - room.hostHitCount,
+		room.hostShot.length != 0
+			? (room.hostHitCount / room.hostShot.length) * 100
+			: room.hostHitCount * 100,
+		room.guestShot.length,
+		room.guestHitCount,
+		room.guestShot.length - room.guestHitCount,
+		room.guestShot.length != 0
+			? (room.guestHitCount / room.guestShot.length) * 100
+			: room.guestHitCount * 100,
+		Date.now() - room.matchStart || 0,
+		room.turnCount
+	);
 
-    socket.to(opponentSocketId).emit(
-        'statResponse',
-        'Completed',
-        room.hostShot.length,
-        room.hostHitCount,
-        room.hostShot.length - room.hostHitCount,
-        room.hostShot.length != 0 ? room.hostHitCount/room.hostShot.length * 100: room.hostHitCount * 100,
-        room.guestShot.length,
-        room.guestHitCount,
-        room.guestShot.length - room.guestHitCount,
-        room.guestShot.length != 0 ? room.guestHitCount/room.guestShot.length * 100: room.guestHitCount * 100,
-        Date.now() - room.matchStart,
-        room.turnCount
-    )
-
+	socket
+		.to(opponentSocketId)
+		.emit(
+			'statResponse',
+			'Completed',
+			room.hostShot.length,
+			room.hostHitCount,
+			room.hostShot.length - room.hostHitCount,
+			room.hostShot.length != 0
+				? (room.hostHitCount / room.hostShot.length) * 100
+				: room.hostHitCount * 100,
+			room.guestShot.length,
+			room.guestHitCount,
+			room.guestShot.length - room.guestHitCount,
+			room.guestShot.length != 0
+				? (room.guestHitCount / room.guestShot.length) * 100
+				: room.guestHitCount * 100,
+			Date.now() - room.matchStart || 0,
+			room.turnCount
+		);
 };
